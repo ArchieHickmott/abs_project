8b521f8bbe5046e5258701278c6221056fb90a7d
archie <25hickmar@gmail.com>
2026-08-26 20:56:24 +1000

    created shell.nix

---
+++ shell.nix
@@ -0,0 +1,60 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+  packages = with pkgs; [
+    # Python
+    python314Full
+
+    # C / C++
+    gcc
+    gdb
+
+    cmake
+    ninja
+
+    pkg-config
+  
+    boost
+    nlohmann_json
+
+    # Crow / Networking
+    crow
+    asio
+    openssl
+    zlib
+
+    # Geospatial / GDAL stack
+    gdal
+    geos
+    proj
+
+    # Development utilities
+    curl
+    git
+
+    valgrind
+
+    httpie
+  ];
+
+  shellHook = ''
+    export PYTHONNOUSERSITE=1
+
+    if [ ! -d .venv ]; then
+      python -m venv .venv
+    fi
+
+    source .venv/bin/activate
+
+    echo ""
+    echo "Python:"
+    python --version
+
+    echo ""
+    echo "C++:"
+    g++ --version | head -n 1
+
+    echo ""
+    echo "CMake:"
+    cmake --version | head -n 1
+  '';
+}
