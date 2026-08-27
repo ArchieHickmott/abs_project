a9338e08c2df54ccc35c28555e5e951592c7704e
archie <25hickmar@gmail.com>
2026-08-27 13:17:50 +1000

    remove serverside geojson storage

--- app/main.py
+++ app/main.py
@@ -7,17 +7,5 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/get-json")
-def get_json():
-    file_path = os.path.join(
-        app.root_path,
-    "test.geojson"
-    )
-
-    return send_file(
-        file_path,
-        mimetype="application/geo+json"
-    )
-
 if __name__ == "__main__":
     app.run(debug=True)
